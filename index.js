978e49d1703fa01af35e69667fb376802ecc282a
Abigail Arisa <abigailarisa1@gmail.com>
2022-09-05 00:26:20 -0500

    commit tree 44043057805f2c79a02d82d12fccec686eb867de

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, driver) {
+return drivers.filter(couldMatch => couldMatch.toUpperCase() === driver.toUpperCase());
+}
+
+function fuzzyMatch(drivers, possibleString ) {
+    return drivers.filter(firstLetter =>firstLetter.toUpperCase().indexOf(possibleString.toUpperCase()) === 0);}
+
+
+function matchName(drivers, possibleName) {
+return drivers = drivers.filter(eachElement => eachElement.name === possibleName);}
